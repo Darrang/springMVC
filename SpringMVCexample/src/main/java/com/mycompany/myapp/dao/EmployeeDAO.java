48082960ce0a785113a932476caa5c2cf200c084
Kimdayoung51 <dayong414@gmail.com>
2021-01-29 16:39:05 +0900

    add java class

---
+++ SpringMVCexample/src/main/java/com/mycompany/myapp/dao/EmployeeDAO.java
@@ -0,0 +1,9 @@
+package com.mycompany.myapp.dao;
+
+import java.util.List;
+
+import com.mycompany.myapp.model.EmployeeVO;
+
+public interface EmployeeDAO {
+	public List<EmployeeVO> getAllEmployees();
+}
---
+++ SpringMVCexample/src/main/java/com/mycompany/myapp/service/EmployeeManager.java
@@ -0,0 +1,9 @@
+package com.mycompany.myapp.service;
+
+import java.util.List;
+
+import com.mycompany.myapp.model.EmployeeVO;
+
+public interface EmployeeManager {
+	public List<EmployeeVO> getAllEmployees();
+}
